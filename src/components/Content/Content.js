d196d6b82f0021d1e0fc38de380fa7fbe1098963
olivia kumar <oliviakumar@gmail.com>
2019-01-14 09:24:25 -0800

    might swap some things out now

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -3,27 +3,20 @@ import { Row, Col, Grid, Card, Button, CardTitle, CardText } from 'reactstrap';
 
 const Content = (props) => {
   return (
-
-    <div className="container_row">
-      <div className="teehee1">
-
+    <div className="container_row" style={{height:"60%"}}>
         <div class="row">
           <Card body>
-            <div class="col-sm-2">
-                <img className="card-img-top" src="https://cdn.shopify.com/s/files/1/1061/1924/products/Nerd_with_Glasses_Emoji_2a8485bc-f136-4156-9af6-297d8522d8d1_large.png" alt="Card image cap"></img>
-            </div>
-
-      <div className="spacer"></div>
-          <div className="teehee"  style={{ paddingLeft: 16 }} >
-            <CardTitle>Special Title Treatment</CardTitle>
-            <CardText>With supportingsupportingsupportingsupportingsupportingsupportingsupporting text below as a natural lead-in to additional content.</CardText>
-            <Button>Go somewhere</Button>
-          </div>
-
+            <Row >
+              <Col xs="3" style={{width:"99%", height:"60%"}}>
+                <img /*style={{width:"50%", height:"60%"}} */className="card-img-top" src="https://cdn.shopify.com/s/files/1/1061/1924/products/Nerd_with_Glasses_Emoji_2a8485bc-f136-4156-9af6-297d8522d8d1_large.png" alt="Card image cap"></img>
+              </Col>
+              <Col xs="9">
+                <CardTitle>Special Title Treatment</CardTitle>
+                <CardText>With supportingsupportingsupportingsupportingsupportingsupportingsupporting text below as a natural lead-in to additional content.</CardText>
+                <Button>Go somewhere</Button>
+              </Col>
+            </Row>
         </Card>
-
-      </div>
-
       </div>
     </div>
 
